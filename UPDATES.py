8b022773210a8fc0b8de5879b3515d144765c447
xllebb <090504opo@gmail.com>
2024-05-10 14:31:57 +0300

    Убрал лишний код

--- UPDATES.py
+++
@@ -1,9 +0,0 @@
-from utils.data import read_file, save_file
-
-data = read_file('data/users.json')
-for user in data:
-    data[user]['Bbalance'] = 0
-    data[user]['Rbalance'] = 0
-    data[user]['miners'] = []
-
-save_file('data/users.json', data)
